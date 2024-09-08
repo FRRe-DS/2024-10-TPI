28c04d8aa5ae5cae78876ada52e7d75358908608
PhilippeDMG <philippedamau@gmail.com>
2024-09-08 10:36:53 -0300

    added shadcn ui library and routes

---
+++ frontend/src/app/edition/[id]/page.tsx
@@ -0,0 +1,5 @@
+
+export default function Page() {
+    return <h1 className="font-bold text-xl mx-auto w-fit">Hello, Next.js!</h1>
+  }
+  
---
+++ frontend/src/app/edition/[id]/voting/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+
+
+
+    return <h1 className="font-bold text-xl mx-auto w-fit">Hello, Next.js!</h1>
+  }
+  
