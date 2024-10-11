637a0e5def16133a62e833aeac7ef4ba5afd5a01
Xacled <carlox7v7@gmail.com>
2024-10-10 22:35:02 -0300

    hola

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,14 +4,12 @@ const Model = dynamic(() => import("@/components/Model"), { ssr: false });
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-2 w-full justify-center items-center bg-slate-400">
-      <div className="col-span-1 flex flex-col items-start ">
-        <h3 className="text-xl font-bold w-fit ml-auto">
-          Bem-vindos a la bienal
-        </h3>
-        <h1 className="text-[5rem] font-bold ml-auto">2024</h1>
+    <div className="flex items-center justify-center w-full h-screen bg-slate-400">
+      <div className="text-left mb-4">
+        <h3 className="text-[5rem] font-bold">Bienal del Chaco</h3>
+        <h1 className=" text-6xl font-bold">2024</h1>
       </div>
-      <div className="col-span-1">
+      <div className="text-[3rem] p-10 m-10">
         <Model />
       </div>
     </div>
