02fc8886c2cf8f91ec39748637ee5c0608f766de
Xacled <carlox7v7@gmail.com>
2024-10-31 20:18:09 -0300

    Admin

---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return <h1 className="font-bold text-xl mx-auto w-fit">Inicio</h1>;
+  }
+  
+  
--- frontend/src/app/admin/sculptors/page.tsx
+++ frontend/src/app/admin/sculptors/page.tsx
@@ -1,4 +1,25 @@
-export default function Page() {
-  return <h1 className="font-bold text-xl mx-auto w-fit">Escultores</h1>;
+import { Payment, columns } from "./columns"
+import { DataTable } from "./data-table"
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      votos: 100,
+      nombre: "Pedro picapiedra",
+      correo: "m@example.com",
+    },
+    // ...
+  ]
 }
 
+export default async function DemoPage() {
+  const data = await getData()
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
+}
