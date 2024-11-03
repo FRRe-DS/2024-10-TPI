59f5eba4bd6c96ffe1f4a48bde53b1da00c08e7f
GalaticPizza <Clastik-mor13@outlook.com.ar>
2024-11-03 18:59:51 -0300

    Actualización de los eventos

--- backend/app/migrations/013_create_table_obras.sql
+++ backend/app/migrations/013_create_table_obras.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS Obras (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	autor_id INT NOT NULL,
-    id_evento INT NOT NULL,
+    id_edicion INT NOT NULL,
 	nombre_obra VARCHAR(255) NOT NULL,
 	descripcion TEXT,
 	tecnica VARCHAR(50),
