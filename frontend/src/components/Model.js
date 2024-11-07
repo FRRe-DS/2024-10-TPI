c089ca152b0241ef71ff567e13214c2af8c2c3d5
JudkevichNatasha <natashanicole@hotmail.com>
2024-11-07 19:26:47 -0300

    Aca pongo UNA SOLA imagen

--- frontend/src/components/Model.js
+++ frontend/src/components/Model.js
@@ -5,7 +5,7 @@ const Model = () => (
   <div id="card">
     <model-viewer
 
-      src="/modelos3D/Zebra.glb"
+      src="modelos3D/este.obj.glb"
 
       ios-src=""
       // poster="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b%2Fposter-astronaut.png?v=1599079951717"
