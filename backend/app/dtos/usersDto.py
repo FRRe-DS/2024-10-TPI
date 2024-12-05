89a9e682e059dc58714bb6dbae2a55e86e51ccce
PhilippeDMG <philippedamau@gmail.com>
2024-12-05 18:09:46 -0300

    a

--- backend/app/dtos/usersDto.py
+++ backend/app/dtos/usersDto.py
@@ -1,43 +1,50 @@
-from pydantic import BaseModel
-from typing import Optional
 from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
 
 # otras clases heredan de esta clase
 class UserBase(BaseModel):
-  id: int
-  nombre: str
-  apellido: str
-  contrasenia_hasheada: str
-  rol: str
-  correo: str
+    id: int
+    nombre: str
+    apellido: str
+    contrasenia_hasheada: str
+    rol: str
+    correo: str
+
 
 # Se usa para validar los datos cuando un nuevo usuario se registra.
 class UserCreate(UserBase):
-  id: Optional[int] = None
+    id: Optional[int] = None
+
 
 class UserUpdate(UserBase):
-  id: Optional[int] = None
-  nombre: Optional[str] = None
-  apellido: Optional[str] = None
-  contrasenia_hasheada: Optional[str] = None
-  rol: Optional[str] = None
-  correo: Optional[str] = None
+    id: Optional[int] = None
+    nombre: Optional[str] = None
+    apellido: Optional[str] = None
+    contrasenia_hasheada: Optional[str] = None
+    rol: Optional[str] = None
+    correo: Optional[str] = None
+
 
 # Esta clase se usa para validar los datos durante el proceso de inicio de sesión de un usuario.
 class UserLogin(UserBase):
-  id: Optional[int] = None
-  nombre: Optional[str] = None
-  apellido: Optional[str] = None
-  contrasenia_hasheada: str
-  correo: str
-  rol: Optional[str] = None
+    id: Optional[int] = None
+    nombre: Optional[str] = None
+    apellido: Optional[str] = None
+    contrasenia_hasheada: str
+    correo: str
+    rol: Optional[str]
+
 
 # Representa a un usuario en el sistema, incluyendo atributos adicionales que podrían ser útiles, como created_at, updated_at, y deleted_at.
 class User(UserBase):
-  id: Optional[int]
-  created_at: datetime
-  updated_at: datetime
-  deleted_at: Optional[datetime]
+    id: Optional[int]
+    created_at: datetime
+    updated_at: datetime
+    deleted_at: Optional[datetime]
 
-  class Config:
-    from_attributes = True
+    class Config:
+        from_attributes = True
+
